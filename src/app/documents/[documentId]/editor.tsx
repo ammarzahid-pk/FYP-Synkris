fe5f5dd9a68c4d8f75b0ff63bb75df699a4b4470
Ammar Zahid <ammarzahid.pk@gmail.com>
2025-09-24 16:26:48 +0500

    Tiptap Extensions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,15 @@
 "use client"
 
-import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import { useEditor, EditorContent } from '@tiptap/react'
 
 export const Editor = () => {
     const editor = useEditor({
@@ -11,8 +19,35 @@ export const Editor = () => {
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 p4-14 pb-10 cursor-text"
             },
         },
-        extensions: [StarterKit],
-        content: '<p>Hello World</p>'
+        extensions: [
+            StarterKit,
+            Image,
+            ImageResize,
+            Table,
+            TableCell,
+            TableHeader,
+            TableRow,
+            TaskItem.configure({
+                nested: true,
+            }),
+            TaskList,
+        ],
+        content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
     })
 
     return ( 
