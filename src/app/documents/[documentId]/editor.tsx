19b182a14f42639577244c5290512542c601cbc5
Ammar Zahid <ammarzahid.pk@gmail.com>
2025-10-01 19:57:50 +0500

    Link And Image Tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -5,6 +5,7 @@ import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
 import Table from '@tiptap/extension-table'
 import Image from '@tiptap/extension-image'
+import Link from '@tiptap/extension-link'
 import { Color } from '@tiptap/extension-color'
 import Highlight from '@tiptap/extension-highlight'
 import FontFamily from '@tiptap/extension-font-family'
@@ -54,6 +55,11 @@ export const Editor = () => {
         },
         extensions: [
             StarterKit,
+            Link.configure({
+              openOnClick: false,
+              autolink: true,
+              defaultProtocol: "https"
+            }),
             Color,
             Highlight.configure({
               multicolor: true,
